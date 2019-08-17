4fe593babf96d04db9a1965b6deaa565ab717b71
Ubuntu <ubuntu@ip-172-31-35-43.us-east-2.compute.internal>
2019-08-17 06:07:57 +0000

    add shell settings

---
+++ shell/aliases.sh
@@ -0,0 +1,21 @@
+# user settings
+# alias
+alias la='ls -a'
+alias ll='ls -l'
+alias lla='ls -la'
+alias sudo='sudo '
+alias upsh='source ~/.bashrc'
+alias c=clear
+alias cls=clear
+alias na=nano
+alias fig='docker compose'
+# alias (git)
+alias gsta='git status'
+alias gadd='git add'
+alias gdif='git diff'
+alias glog='git log'
+alias gcom='git commit'
+alias gpus='git push'
+alias gpul='git pull'
+alias gclo='git clone'
+alias gche='git checkout'
